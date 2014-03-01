9fbea5a6516df5c7771fb2c04d4cb989808be5e1
Omar Thanawalla <omar.thanawalla@gmail.com>
2014-02-28 19:24:50 -0600

    able to push

---
+++ SocialPlanMaking/CalendarContainerVC.h
@@ -0,0 +1,13 @@
+//
+//  CalendarContainerVC.h
+//  SocialPlanMaking
+//
+//  Created by Omar Thanawalla on 2/28/14.
+//  Copyright (c) 2014 Omar Thanawalla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalendarContainerVC : UIViewController
+
+@end
---
+++ SocialPlanMaking/CalendarContainerVC.m
@@ -0,0 +1,43 @@
+//
+//  CalendarContainerVC.m
+//  SocialPlanMaking
+//
+//  Created by Omar Thanawalla on 2/28/14.
+//  Copyright (c) 2014 Omar Thanawalla. All rights reserved.
+//
+
+#import "CalendarContainerVC.h"
+
+@interface CalendarContainerVC ()
+
+@end
+
+@implementation CalendarContainerVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    NSLog(@"prepareForSegue: %@",    segue.identifier);
+}
+
+@end
